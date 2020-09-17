9ae59d59701c49c372135a4019230ab5c502bc16
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-09-17 10:26:24 +0630

    Add project files.

---
+++ HotelKoKoMu_CardRegister/Controllers/CardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelKoKoMu_CardRegister.Controllers
+{
+    public class CardController : Controller
+    {
+        // GET: Card
+        public ActionResult CargRegisterPage1()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelKoKoMu_CardRegister/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotelKoKoMu_CardRegister.MvcApplication" Language="C#" %>

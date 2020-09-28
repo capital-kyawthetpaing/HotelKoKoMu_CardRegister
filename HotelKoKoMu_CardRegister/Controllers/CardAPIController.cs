a524ec6e2d7972b6213694548701a3fb9999e3dd
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-09-28 15:33:49 +0630

    omk

--- HotelKoKoMu_CardRegister/Controllers/CardAPIController.cs
+++ HotelKoKoMu_CardRegister/Controllers/CardAPIController.cs
@@ -11,14 +11,18 @@ using System.Data;
 using System.Configuration;
 using NpgsqlTypes;
 using System.Web;
+using System.Net.Http.Formatting;
+using System.Web.Http.ModelBinding;
 
 namespace HotelKoKoMu_CardRegister.Controllers
 {
-   
+
     public class CardAPIController : ApiController
     {
         string conStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         string culture = HttpContext.Current.Request.Cookies["culture"].Value;
+
+        #region 
         [HttpPost]
         [ActionName("SaveCardInformation")]
         public IHttpActionResult SaveCardInformation(CardRegisterModel model)
@@ -29,9 +33,9 @@ namespace HotelKoKoMu_CardRegister.Controllers
             if (model.Mode == "1")
             {
                 #region for keyboard input
-               
+
                 model.Sqlprms[0] = new NpgsqlParameter("@guestName", model.GuestName);
-                if(culture=="Ja")
+                if (culture == "Ja")
                     model.Sqlprms[1] = new NpgsqlParameter("@kanaName", model.KanaName);
                 else
                     model.Sqlprms[1] = new NpgsqlParameter("@kanaName", model.GuestName);
@@ -55,7 +59,7 @@ namespace HotelKoKoMu_CardRegister.Controllers
                 #region for hand writing
 
                 model.Sqlprms[0] = new NpgsqlParameter("@guestNameimg", ConvertBase64StringToByte(model.GuestName));
-                if(culture=="Ja")
+                if (culture == "Ja")
                     model.Sqlprms[1] = new NpgsqlParameter("@kanaNameimg", ConvertBase64StringToByte(model.KanaName));
                 else
                     model.Sqlprms[1] = new NpgsqlParameter("@kanaNameimg", ConvertBase64StringToByte(model.GuestName));
@@ -83,11 +87,64 @@ namespace HotelKoKoMu_CardRegister.Controllers
             model.Sqlprms[15] = new NpgsqlParameter("@updateddate", currentDate);
             return Ok(bdl.InsertUpdateDeleteData(cmdText, model.Sqlprms));
         }
+        #endregion
+
+        //[HttpPost]
+        //[ActionName("SaveCardInformation")]
+        //public IHttpActionResult SaveCardInformation(CardRegisterModel model)
+        //{
+        //    #region
+        //    string cmdText = "CALL saveguestinfo(@guestname,@guestnamehw,@kananame,@kananamehw,@postalcode,@postalcodehw,@phoneno,@phonenohw,@address1,@addresshw1,@address2,@addresshw2,@workplace,@workplacehw,@nationality,@nationalityhw,@passportno,@passportnohw,@sign,@arrivaldate,@departuredate,@creator,@updator)";            
+        //    BaseDL bdl = new BaseDL();
+        //    model.Sqlprms = new NpgsqlParameter[23];
+
+        //    model.Sqlprms[0] = model.Mode == "1" ? new NpgsqlParameter("@guestname", model.GuestName) : new NpgsqlParameter("@guestname", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[1] = model.Mode == "2" ? new NpgsqlParameter("@guestnamehw", ConvertBase64StringToByte(model.GuestName)) : new NpgsqlParameter("@guestnamehw", NpgsqlDbType.Bytea) { Value=""};
+        //    if (culture == "Ja")
+        //        model.Sqlprms[2] = model.Mode == "1" ? new NpgsqlParameter("@kananame", model.KanaName) : new NpgsqlParameter("@kananame", NpgsqlDbType.Varchar) { Value = "" };
+        //    else
+        //        model.Sqlprms[2] = model.Mode == "1" ? new NpgsqlParameter("@kananame", model.GuestName) : new NpgsqlParameter("@kananame", NpgsqlDbType.Varchar) { Value = "" };
+
+        //    if (culture == "Ja")
+        //        model.Sqlprms[3] = model.Mode == "2" ? new NpgsqlParameter("@kananamehw", ConvertBase64StringToByte(model.KanaName)) : new NpgsqlParameter("@kananamehw", NpgsqlDbType.Bytea) { Value = "" };
+        //    else
+        //        model.Sqlprms[3] = model.Mode == "2" ? new NpgsqlParameter("@kananamehw", ConvertBase64StringToByte(model.GuestName)) : new NpgsqlParameter("@kananamehw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[4] = model.Mode == "1" ? new NpgsqlParameter("@postalcode", model.PostalCode) : new NpgsqlParameter("@postalcode", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[5] = model.Mode == "2" ? new NpgsqlParameter("@postalcodehw", ConvertBase64StringToByte(model.PostalCode)) : new NpgsqlParameter("@postalcodehw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[6] = model.Mode == "1" ? new NpgsqlParameter("@phoneno", model.PhoneNo) : new NpgsqlParameter("@phoneno", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[7] = model.Mode == "2" ? new NpgsqlParameter("@phonenohw", ConvertBase64StringToByte(model.PhoneNo)) : new NpgsqlParameter("@phonenohw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[8] = model.Mode == "1" ? new NpgsqlParameter("@address1", model.Address1) : new NpgsqlParameter("@address1", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[9] = model.Mode == "2" ? new NpgsqlParameter("@addresshw1", ConvertBase64StringToByte(model.Address1)) : new NpgsqlParameter("@addresshw1", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[10] = model.Mode == "1" ? new NpgsqlParameter("@address2", model.Address2) : new NpgsqlParameter("@address2", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[11] = model.Mode == "2" ? new NpgsqlParameter("@addresshw2", ConvertBase64StringToByte(model.Address2)) : new NpgsqlParameter("@addresshw2", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[12] = model.Mode == "1" ? new NpgsqlParameter("@workplace", model.WorkPlace) : new NpgsqlParameter("@workplace", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[13] = model.Mode == "2" ? new NpgsqlParameter("@workplacehw", ConvertBase64StringToByte(model.WorkPlace)) : new NpgsqlParameter("@workplacehw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[14] = model.Mode == "1" ? new NpgsqlParameter("@nationality", model.Nationality) : new NpgsqlParameter("@nationality", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[15] = model.Mode == "2" ? new NpgsqlParameter("@nationalityhw", ConvertBase64StringToByte(model.Nationality)) : new NpgsqlParameter("@nationalityhw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[16] = model.Mode == "1" ? new NpgsqlParameter("@passportno", model.Passport) : new NpgsqlParameter("@passportno", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[17] = model.Mode == "2" ? new NpgsqlParameter("@passportnohw", ConvertBase64StringToByte(model.Passport)) : new NpgsqlParameter("@passportnohw", NpgsqlDbType.Bytea) { Value = "" };
+
+        //    model.Sqlprms[18] = new NpgsqlParameter("@sign", ConvertBase64StringToByte(model.Sign));
+        //    model.Sqlprms[19] = new NpgsqlParameter("@arrivaldate", NpgsqlDbType.Date) { Value=model.ArrivalDate.Date};
+        //    model.Sqlprms[20] = new NpgsqlParameter("@departuredate", NpgsqlDbType.Date) { Value = model.DepartureDate.Date };
+        //    model.Sqlprms[21] = new NpgsqlParameter("@creator", NpgsqlDbType.Varchar) { Value = "" };
+        //    model.Sqlprms[22] = new NpgsqlParameter("@updator", NpgsqlDbType.Varchar) { Value = "" };
+        //    return Ok(bdl.InsertUpdateDeleteData(cmdText, model.Sqlprms));
+        //    #endregion
+        //}
+
 
         //to convert base64string to byte 
         public byte[] ConvertBase64StringToByte(string common)
         {
-            byte[] byteCommon=null;
+            byte[] byteCommon = null;
             if (!string.IsNullOrEmpty(common))
             {
                 string[] arrCommon = common.Split(',');
@@ -104,7 +161,7 @@ namespace HotelKoKoMu_CardRegister.Controllers
             BaseDL bdl = new BaseDL();
             hotelModel.Sqlprms = new NpgsqlParameter[1];
             hotelModel.Sqlprms[0] = new NpgsqlParameter("@hotelno", hotelModel.HotelNo);
-            string cmdText = "Select * from mst_hotel where hotel_no=@hotelno";
+            string cmdText = "Select hotel_name,logo_data from mst_hotel where hotel_no=@hotelno";
             DataTable dt = bdl.SelectDataTable(cmdText, hotelModel.Sqlprms);
             return Ok(dt);
         }
